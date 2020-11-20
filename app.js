6ebe179a1a18ba51daf985ae61f0f27cb210a84a
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-11-20 17:50:01 -0600

    finished 9.1

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+	profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
